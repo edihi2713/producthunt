1ede4609792ba190729a01778e0fb0b68c3ed26f
Édison Arley Higuita Higuita <edihi2713@gmail.com>
2016-10-27 12:02:13 -0500

    Crea la ruta /products/:id/edit

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
  post '/products', to: 'products#create' 
 
- get '/products/:id', to: 'products#show'
+ get '/products/:id', to: 'products#show', as: 'product'
+
+ get '/products/:id/edit', to: 'products#edit'
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
